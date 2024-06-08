b1b848b16bf703292957ea40024cef97b84d4593
BrantLauro <brantlauro@gmail.com>
2024-06-08 17:43:25 -0300

    header navbar

--- resources/views/main/home.blade.php
+++ resources/views/main/home.blade.php
@@ -3,14 +3,18 @@
 
 @section('main')
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">Navbar scroll</a>
+
+<header class="p-3 mb-3 border-bottom">
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
+        <img src="{{ asset('images/logo-header-webuy.png')}}" alt="logo webuy" width="100" height="35">
+      </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarScroll">
-        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <ul class="navbar-nav me-auto my-2 my-lg-0">
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
@@ -32,14 +36,49 @@
             <a class="nav-link disabled" aria-disabled="true">Link</a>
           </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-indigo" type="submit">Search</button>
-        </form>
+        <div class="d-flex align-itens-center">
+          <form class="d-flex me-2" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+          <div class="dropdown text-end">
+            <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+              <img src="{{ asset('images/people.svg')}}" alt="perfil" width="40" height="40" class="rounded-circle">
+            </a>
+            <ul class="dropdown-menu text-small">
+              <li><a class="dropdown-item" href="#">New project...</a></li>
+              <li><a class="dropdown-item" href="#">Settings</a></li>
+              <li><a class="dropdown-item" href="#">Profile</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Sign out</a></li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </nav>
+</header>
 
-  Texto
+
+
+<div class="container">
+  <div class="row" data-masonry='{"percentPosition": true }'>
+    <div class="col-sm-6 col-lg-4 mb-4">
+      <div class="card">
+        <img src="{{ asset('images/renan.jpeg')}}" alt="" class="rounded">
+        <div class="card-body">
+          <h5 class="card-title">Renan Ciríaco</h5>
+          <p class="card-text">R$ 100,00</p>
+          <div class="d-grid gap-2 mx-auto">
+            <button class="btn btn-indigo">Ver</button>
+            <button class="btn btn-outline-dark">Adicionar ao Carrinho</button>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</div>
 
 @endsection
