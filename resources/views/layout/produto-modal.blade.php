d178e6dcc5fb20de2e6ec7a70641a6165cca351a
BrantLauro <brantlauro@gmail.com>
2024-06-10 18:48:48 -0300

    modal atualizado

--- resources/views/layout/produto-modal.blade.php
+++ resources/views/layout/produto-modal.blade.php
@@ -1,12 +1,25 @@
 <div class="modal fade" id="produto-modal" tabindex="-1" aria-labelledby="produto-modal-label" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg">
-      <div class="modal-content rounded-4 shadow">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+      <div class="modal-content d-flex rounded-3 py-2 px-3 shadow">
         <div class="modal-header border-bottom-0">
-          <h1 class="modal-title fs-5">Modal title</h1>
+          <h1 class="modal-title h2">Feijão Carioca KG</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body py-0">
-          <p>This is a modal sheet, a variation of the modal that docs itself to the bottom of the viewport like the newer share sheets in iOS.</p>
+            <div class="row">
+                <div class="col">
+                    <div class="text-center">
+                        <img src="{{ asset('images/exemple.png')}}" alt="" class="img-fluid rounded">
+                    </div>
+                </div>
+                <div class="col">
+                    <h2 class="h2">R$ 05,00</h2>
+                    <input type="number" name="" id="">
+                    <p>Descubra o sabor e a tradição do Feijão Carioca, um dos alimentos mais populares e apreciados na culinária brasileira. Nosso feijão é selecionado com rigor, garantindo grãos de alta qualidade, frescos e saborosos. Com sua cor bege e listras marrons características, o Feijão Carioca é a base perfeita para diversas receitas, desde o clássico arroz e feijão até sopas e saladas nutritivas.</p>
+                    <p>Alimento não perecível</p>
+                </div>
+            </div>
+           
         </div>
         <div class="modal-footer flex-column align-items-stretch w-100 gap-2 pb-3 border-top-0">
           <button type="button" class="btn btn-lg btn-primary">Save changes</button>
