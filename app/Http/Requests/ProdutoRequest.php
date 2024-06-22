db14096ec31dcb05eee3cf3535f907fc87855c1b
thiagorezendev <thiagor3zende@gmail.com>
2024-06-21 22:06:25 -0300

    att

--- app/Http/Requests/ProdutoRequest.php
+++ app/Http/Requests/ProdutoRequest.php
@@ -23,6 +23,7 @@ class ProdutoRequest extends FormRequest{
         return [
             'nome_produto' => 'required|string',
             'desc_produto' => 'required|string',
+            'id_categoria' => 'required|numeric',
             'foto_produto' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
             'preco_produto' => 'required|numeric',
         ];
@@ -33,6 +34,7 @@ class ProdutoRequest extends FormRequest{
         return [
             'nome_produto.required' => 'O campo nome do produto é obrigatório',
             'desc_produto.required' => 'O campo descrição do produto é obrigatório',
+            'id_categoria.required' => 'O campo categoria do produto é obrigatório',
             'foto_produto.required' => 'O campo foto do produto é obrigatório',
             'preco_produto.required' => 'O campo preço do produto é obrigatório',
             'preco_produto.numero' => 'O campo preço do produto deve ser um número',
